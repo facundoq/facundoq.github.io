92d61126e2d0171f1190f8556fd776e9147c2549
facundoq <facundoq@gmail.com>
2023-08-23 13:24:11 -0300

    updated gem

--- Gemfile
+++ Gemfile
@@ -11,3 +11,4 @@ group :jekyll_plugins do
     gem 'jekyll-feed'
     gem 'jekyll-seo-tag'
   end
+gem "webrick", "~> 1.8"
--- compile.sh
+++ compile.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+
+echo "Make sure 'ruby', 'ruby-dev' and 'ruby-bundler' have been installed, and run `bundle install` before running this command."
 bundle exec jekyll serve
